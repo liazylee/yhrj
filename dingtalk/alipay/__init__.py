4bad6543a1680dc7bc2c7a2a0cd2808dedb827c6
zhenyili <lizhenyi1314>
2022-02-09 20:33:05 +0800

    add：uwsgi socket connect

---
+++ dingtalk/alipay/__init__.py
@@ -0,0 +1,20 @@
+"""
+@author:liazylee
+@license: Apache Licence
+@time: 2022/2/9 19:38
+@contact: li233111@gmail.com
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
